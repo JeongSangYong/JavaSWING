05931cfde760b31b49ea1f0e6851066cacd18e7e
SuperClass <SuperClass>
2015-07-30 15:21:10 +0900

    2015.07.30 SWING(GUI)

---
+++ src/swing/LottoUI.java
@@ -0,0 +1,17 @@
+package swing;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JFrame;
+
+public class LottoUI extends JFrame implements ActionListener{
+
+	public LottoUI() {
+		
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {	
+		
+	}	
+}
