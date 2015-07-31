55c6efe33c79ac6b97956f0bf2fb7f74a9f5fbe2
SuperClass <SuperClass>
2015-07-31 15:02:13 +0900

    2015.07.31 GUI(Lotto)

--- src/swing/Lotto.java
+++ src/swing/Lotto.java
@@ -1,5 +1,49 @@
 package swing;
 
-public class Lotto {
+import java.util.Arrays;
 
+public class Lotto {
+	//필드
+	int[] lotto = new int[6];
+
+	//생성자
+	
+	public int[] getLotto() {
+		return lotto;
+	}
+
+	//로또는 외부에서 숫자를 주입받는 것이 아니라 자체적으로
+	//랜덤숫자를 발생시켜야 한다.
+	public void setLotto() {		
+		for (int i = 0; i < lotto.length; i++) {
+			lotto[i] = 0;	//배열내부의 지변초기화
+		}
+		for (int i = 0; i < lotto.length; i++) {
+			int randomNum = (int) ((Math.random()*45)+1);
+			
+			//로또는 숫자가 숭복이 있어서는 안됨
+			//나중에 나온 숫자가 먼저 나온 숫자와 같다면 그 숫자는 폐기해야함
+			boolean exist = false;
+			for (int j = 0; j < lotto.length; j++) {
+				if (randomNum == lotto[j]) {
+					exist = true;
+					break;
+				}
+			}
+			if (exist) {
+				i--;
+				continue;
+			}else{
+				lotto[i]=randomNum;
+			}
+		}
+		//중복이 되지 않은 6개의 랜덤숫자가 배열에 담겼다면
+		//그 배열의 숫자를 오름차순으로 정렬해야 함
+		Arrays.sort(lotto);
+	}
+	
+	//멤버메소드
+	
+	
+	
 }
--- src/swing/LottoUI.java
+++ src/swing/LottoUI.java
@@ -1,17 +1,71 @@
 package swing;
 
+import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class LottoUI extends JFrame implements ActionListener{
 
+	/**
+	 * serialVersionUID 는 시리얼 번호로 클래스 구분 식별번호
+	 */
+	private static final long serialVersionUID = 1L;
+	Lotto lotto;
+	JButton btn;
+	JPanel panelNorth, panelSouth;
+	ImageIcon icon;
+	List<JButton> btns;
+	
 	public LottoUI() {
+		setTitle("로또 발생기");
+		lotto = new Lotto();
+		btns = new ArrayList<JButton>();
+		panelNorth = new JPanel();
+		panelSouth = new JPanel();
+		btn = new JButton("로또 번호 추첨");
 		
+		btn.addActionListener(this);//this는 큰 객체인 프레임을 가리킴
+		panelNorth.add(btn);
+		add(panelNorth, BorderLayout.NORTH);
+		add(panelSouth, BorderLayout.SOUTH);
+		setBounds(300, 400, 1200, 300);
+		setVisible(true);
 	}
 	
 	@Override
 	public void actionPerformed(ActionEvent e) {	
-		
+		//버튼 객체 6개를 만듬
+		if(btns.size() == 0){
+			JButton tmp = null;
+			for (int i = 0; i < 6; i++) {
+				tmp = new JButton();
+				btns.add(tmp);
+				panelSouth.add(tmp);
+			}
+		}
+		//6개의 랜덤숫자르 만듬
+		lotto.setLotto();
+		int[] arr = lotto.getLotto();
+/*		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i]+"\t");
+		}*/
+		//6개의 버튼 객체에 아이콘을 붙인다.
+		for (int i = 0; i < arr.length; i++) {
+			btns.get(i).setIcon(getIcon(arr[i]));
+		}
+	}
+
+	private Icon getIcon(int i) {
+		String imgPath = "src/images/"+Integer.toString(i)+".gif";
+		return new ImageIcon(imgPath);
 	}	
 }
